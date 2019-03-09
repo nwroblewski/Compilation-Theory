a0e799c1162942f4dae637a2448115d1da304e01
nwroblewski <nwroblewski@pop-os.localdomain>
2019-03-09 21:34:56 +0100

    Numbers rules added

--- lab1/scanner.py
+++ lab1/scanner.py
@@ -17,9 +17,27 @@ reserved = {
 }
 
 tokens = ("DOTADD","DOTSUB","DOTMUL","DOTDIV","ADDEQ","SUBEQ","MULEQ","DIVEQ",
-        "LEQ","GEQ","NEQ","EQ","ID","FLOATNUM","INTNUM","STRING") + list(reserved.values())
+        "LEQ","GEQ","NEQ","EQ","ID","FLOATNUM","INTNUM","STRING","MATRIX") + list(reserved.values())
+
+
+
+
+def t_FLOATNUM(t):
+     r'(([0-9]+)(\.[0-9]+))'
+    t.value = float(t.value)
+    return t
+
+
+def t_INTNUM(t):
+    r"\d+"
+    t.value = int(t.value)
+    return t
 
 
 def t_newline(t):
      r'\n+'
      t.lexer.lineno += len(t.value)
+
+
+def p_error(p):
+    print("parsing error\n")
